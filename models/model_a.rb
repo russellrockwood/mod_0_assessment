ff5065e242096f41db4548e3182fb3ddb1e2439b
Russell Rockwood <russell.rockwood@gmail.com>
2021-09-24 13:28:39 -0500

    Add dinosaur hash

---
+++ models/model_a.rb
@@ -0,0 +1,10 @@
+dinosaur = {
+  color: 'green',
+  name: 'big dino',
+  height: 98,
+  weight: 2938,
+  is_friendly: true,
+  eats_people: true,
+  behaviors: ['runs fast', 'eats a lot', 'drinks red bull'],
+  locations: ['Northern Europe', 'Russia', 'China']
+}
