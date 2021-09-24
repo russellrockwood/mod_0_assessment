eeb08a3a47186ac54d26031590c24c4232f7ab84
Russell Rockwood <russell.rockwood@gmail.com>
2021-09-24 13:36:14 -0500

    Add developer hash

---
+++ models/model_b.rb
@@ -0,0 +1,10 @@
+developer_1 = {
+  first_name = 'Russell',
+  last_name = 'Rockwood',
+  id_num = 938745,
+  salary = 45757.98,
+  languages = ['Ruby', 'Python', 'C#'],
+  frameworks = ['Django', '.Net', 'Rails'],
+  remote_position = true,
+  is_contractor = false
+}
