e9c277e97c104e675bedcb196255acdba5d967ca
Russell Rockwood <russell.rockwood@gmail.com>
2021-09-24 13:21:09 -0500

    Complete program specific challenge

---
+++ variables.rb
@@ -0,0 +1,12 @@
+movie = 'Platoon'
+is_entertaining = true
+movie = 'JFK'
+
+num_4 = 4
+num_6 = 6
+product = num_4 * num_6
+
+favorite_foods = ['pizza', 'carrots', 'chips']
+favorite_foods.shift()
+
+p movie.length
